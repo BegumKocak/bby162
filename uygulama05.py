e0eedc14abe2688418c826196de0d27ca1221396
BegumKocak <36486341+BegumKocak@users.noreply.github.com>
2018-04-10 15:43:33 +0300

    Add files via upload

---
+++ uygulama05.py
@@ -0,0 +1,29 @@
+import random
+hak = 3
+i = 0
+kelimeler = random.choice(["telefon","telgraf","internet","televizyon","gazete"])
+kelimeklavuzu = []
+for a in kelimeler:
+    kelimeklavuzu.append("-")
+print(kelimeklavuzu)
+
+
+while hak > 0:
+    alınanharf = input("Bir harf giriniz: ")
+    if alınanharf in kelimeler:
+        for kontrolet in kelimeler:
+            if kelimeler [i] == alınanharf:
+                kelimeklavuzu [i] = alınanharf
+            i+=1
+        print(kelimeklavuzu)
+        i=0
+
+
+
+    else:
+        i=0
+        hak-=1
+        print('Yanlış harf girdiniz. Kalan hakkınız: {}'.format(hak))
+
+if hak == 0:
+    print('Oyunu kaybettiniz. Doğru kelime "{}".\n'.format(kelimeler))
